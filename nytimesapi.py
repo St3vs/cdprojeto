f0c1d8055b317ab5c3f411e041ff453cf29969f8
Rodrigo Esteves <97702882+St3vs@users.noreply.github.com>
2025-04-03 14:55:57 +0100

    Update nytimesapi.py

--- nytimesapi.py
+++ nytimesapi.py
@@ -2,11 +2,13 @@ import requests
 import json
 import logging
 import os
+import time
 from datetime import datetime
+import schedule
 
 # Configuração do logging para registrar apenas o dia e hora
 logging.basicConfig(
-    filename="scraper.log",
+    filename="logging.log",
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M"  # Formato de data e hora (ano-mês-dia hora:minuto)
@@ -14,18 +16,19 @@ logging.basicConfig(
 
 # Chave da API do NYT
 API_KEY = "mtE9UItUjdAMkiAuOo0ljc9P60647UqL"
-URL = f"https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key={API_KEY}"
+URL = f"https://api.nytimes.com/svc/news/v3/content/section/front.json?api-key={API_KEY}"
 
 # Nome do arquivo JSON para armazenar os artigos
-JSON_FILE = "nyt_articles.json"
+JSON_FILE = "dados.json"
 
 def obter_dados():
-    """Obtém os dados da API do NYT e retorna uma lista de artigos"""
+    """Obtém os dados da página inicial do NYT e retorna uma lista de artigos"""
     try:
         response = requests.get(URL)
-        response.raise_for_status()  # Lança erro se a resposta for ruim (ex: 404, 500)
+        response.raise_for_status()
         data = response.json()
 
+        # Extrai os artigos da resposta
         return data.get("results", [])  # Retorna lista vazia se "results" não existir
     except requests.exceptions.RequestException as e:
         logging.error(f"Erro na requisição à API: {e}")
@@ -38,17 +41,17 @@ def carregar_json():
             try:
                 return json.load(f)
             except json.JSONDecodeError:
-                logging.error("Erro ao carregar o JSON, arquivo pode estar corrompido.")
+                logging.error("Erro ao carregar o JSON.")
                 return []
     return []
 
 def salvar_json(data):
-    """Salva os dados no arquivo JSON"""
+    """Guarda os dados no arquivo JSON"""
     with open(JSON_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 def atualizar_dados():
-    """Obtém novos artigos e atualiza o JSON sem duplicar"""
+    """Obtém os novos artigos da página inicial e atualiza o JSON sem duplicar"""
     novos_dados = obter_dados()
     if not novos_dados:
         logging.warning("Nenhum dado novo encontrado.")
@@ -71,10 +74,11 @@ def atualizar_dados():
 
     salvar_json(artigos_existentes)
 
-    # Log usando o horário padrão do sistema, apenas com dia e hora
-    agora = datetime.now().strftime("%Y-%m-%d %H:%M")  # Somente dia e hora
+    # Log atrvés do horário padrão do sistema, apenas com dia e hora
+    agora = datetime.now().strftime("%Y-%m-%d %H:%M") 
     logging.info(f"{agora} - Adicionados {novos_adicionados} artigos. Total: {len(artigos_existentes)}")
 
 # Executar o script
 if __name__ == "__main__":
     atualizar_dados()
+
